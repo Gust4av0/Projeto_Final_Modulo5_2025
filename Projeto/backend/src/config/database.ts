5fc928a8d7ad3dcc2b303b99d18c04101dda1292
Gust4av0 <gustavomarcolin2005@gmail.com>
2025-06-05 21:45:07 -0300

    mexendo no docker file

--- Projeto/backend/src/config/database.ts
+++ Projeto/backend/src/config/database.ts
@@ -5,9 +5,10 @@ const sequelize = new Sequelize(
   process.env.DB_USER || "root",
   process.env.DB_PASSWORD || "",
   {
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "localhost",  // Certifique-se de que 'database' está correto em Docker Compose
     port: Number(process.env.DB_PORT) || 3306,
     dialect: "mysql",
+    logging: false, // Opcional, mas útil para evitar logs excessivos no console
   }
 );
 
